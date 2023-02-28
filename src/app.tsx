5f6ce9a5ad5c78fbc1767ac650b95f57c22e054c
McVyp <93302780+McVyp@users.noreply.github.com>
2023-02-28 22:23:30 +0545

    added Drawer

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { Text, View } from "react-native";
+import Navigations from "./navs";
 
 
 const App =() => {
     return(
         <NavigationContainer>
-            <View style={{
-                flex: 1,
-                backgroundColor: 'white',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}>
-                <Text>Hello, There</Text>
-            </View>
+            <Navigations />
         </NavigationContainer>
     )
 }
