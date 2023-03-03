d482d2ce26326eb14a440ee76f78a29140cafd0d
McVyp <93302780+McVyp@users.noreply.github.com>
2023-03-04 00:12:58 +0545

    added Drawer toggle

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, {useCallback} from "react";
 import { Box, Container, Text, TouchableOpacity} from "../atoms";
 import NoteList from "../components/note-list";
 import HeaderBar from "../components/header-bar";
 import FeatherIcon from "../components/icon";
+import { CompositeScreenProps } from "@react-navigation/native";
+import { DrawerScreenProps } from "@react-navigation/drawer";
+import { HomeDrawerParamList, RootStackParamList } from "../navs";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-export default function MainScreen() {
+type Props = CompositeScreenProps<
+    DrawerScreenProps<HomeDrawerParamList, 'Main'>,
+    NativeStackScreenProps<RootStackParamList>
+>
+export default function MainScreen({navigation}: Props) {
+    const handleSidebarToggle = useCallback(()=> {
+        navigation.toggleDrawer()
+    },[navigation])
     return(
         <Container justifyContent="center" alignItems="center">
             <NoteList />
             <HeaderBar>
-                <TouchableOpacity m="xs" p="xs" rippleBorderless>
+                <TouchableOpacity m="xs" p="xs" rippleBorderless onPress={handleSidebarToggle}>
                     <FeatherIcon name="menu" size={22} />
                 </TouchableOpacity>
                 <Box flex={1} alignItems="center">
