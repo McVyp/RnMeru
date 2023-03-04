b1074fd33dd3b185315eb2874ee09d3df3c6409d
McVyp <93302780+McVyp@users.noreply.github.com>
2023-03-04 11:32:22 +0545

    Added sticky-header

--- src/atoms/container.tsx
+++ src/atoms/container.tsx
@@ -3,8 +3,11 @@ import { BoxProps } from '@shopify/restyle';
 import { Theme } from '../themes';
 import Box from './box';
 
+type Props = BoxProps<Theme> & {
+    children: React.ReactNode
+}
 
-const Container : React.FC<BoxProps<Theme>> = props => (
+const Container : React.FC<Props> = props => (
     <Box {...props} flex={1} backgroundColor='$background'>
         {props.children}
     </Box>
